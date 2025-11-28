58291a725938ef5b2feee98dc6f67f9535e735c1
Ojas Maurya <ojasmaurya30@gmail.com>
2025-11-29 01:41:37 +0530

    O(n) solution

--- src/Sum_of_Subarray_Minimums.java
+++ src/Sum_of_Subarray_Minimums.java
@@ -1,80 +1,77 @@
 import java.util.*;
 public class Sum_of_Subarray_Minimums {
 
-    /* Following is the Brute Fore Solution*/
-
-    public static int sumSubarrayMins(int[] arr) {
-        int sum = 0;
-        int mod = (int)1e9 + 7;
-        for(int i=0; i<arr.length; i++){
-            int min = arr[i];
-            for(int j=i; j<arr.length; j++){
-                min = Math.min(min, arr[j]);
-                sum += min;
-            }
-        }
-        sum = sum%mod;
-        return sum;
-    }
-
-    public static int findNSE(int[] arr, int i){
+    public static int[] findNSE(int[] arr){
         Stack<Integer> st = new Stack<>();
-        int nse = arr.length;
-        for(int j=arr.length-1; j>=i; j--){
+        int[] ans = new int[arr.length];
+        int n = arr.length;
+        st.push(n-1);
+        ans[n-1] = n;
+        for(int i=n-2; i>=0; i--){
             if(!st.isEmpty() && arr[st.peek()] < arr[i]){
-                nse = st.peek();
-                st.push(j);
+                ans[i] = st.peek();
+                st.push(i);
             }else{
                 while(!st.isEmpty()){
                     st.pop();
                     if(!st.isEmpty() && arr[st.peek()] < arr[i]){
-                        nse = st.peek();
-                        st.push(j);
+                        ans[i] = st.peek();
+                        st.push(i);
                         break;
                     }
                 }
                 if(st.isEmpty()){
-                    st.push(j);
+                    st.push(i);
+                    ans[i] = n;
                 }
             }
         }
-        return nse-i;
+        return ans;
     }
 
-    public static int findPSEE(int[] arr, int i){
+    public static int[] findPSEE(int[] arr){
         Stack<Integer> st = new Stack<>();
-        int psee = -1;
-        for(int j=0; j<=i; j++){
+        int[] ans = new int[arr.length];
+        int n = arr.length;
+        ans[0] = -1;
+        st.push(0);
+        for(int i=1; i<n; i++){
             if(!st.isEmpty() && arr[st.peek()] <= arr[i]){
-                psee = st.peek();
-                st.push(j);
+                ans[i] = st.peek();
+                st.push(i);
             }else{
                 while(!st.isEmpty()){
                     st.pop();
                     if(!st.isEmpty() && arr[st.peek()] <= arr[i]){
-                        psee = st.peek();
-                        st.push(j);
+                        ans[i] = st.peek();
+                        st.push(i);
                         break;
                     }
                 }
                 if(st.isEmpty()){
-                    st.push(j);
+                    st.push(i);
+                    ans[i] = -1;
                 }
             }
         }
-        return i-psee;
+        return ans;
+    }
+
+    public static int sumSubarrayMins(int[] arr) {
+        int[] nse = findNSE(arr);
+        int[] psee = findPSEE(arr);
+        long sum = 0;
+        int mod = (int)1e9 + 7;
+        for(int i=0; i<arr.length; i++){
+            long ans = (long)(nse[i] - i)*(i - psee[i]);
+            sum = (sum + ans * arr[i]) % mod;
+        }
+        return (int)sum;
     }
 
     public static void main(String[] args) {
         int[] arr = {3,1,2,4};
-        double sum = 0;
-        int mod = (int)1e9 + 7;
-        for(int i=0; i<arr.length; i++){
-            int nse = findNSE(arr, i);
-            int pse = findPSEE(arr, i);
-            sum += ((nse*pse)*arr[i]);
-        }
-        sum = sum%mod;
-        System.out.println((int)sum);
+        System.out.println(sumSubarrayMins(arr));
     }
+
 }
