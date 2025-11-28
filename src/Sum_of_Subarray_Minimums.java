27a58880770d60060e868887db69e0e2cc0bf0c4
Ojas Maurya <ojasmaurya30@gmail.com>
2025-11-28 23:34:56 +0530

    Optimal solution using Stack

--- src/Sum_of_Subarray_Minimums.java
+++ src/Sum_of_Subarray_Minimums.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Sum_of_Subarray_Minimums {
 
     /* Following is the Brute Fore Solution*/
@@ -15,8 +16,65 @@ public class Sum_of_Subarray_Minimums {
         sum = sum%mod;
         return sum;
     }
+
+    public static int findNSE(int[] arr, int i){
+        Stack<Integer> st = new Stack<>();
+        int nse = arr.length;
+        for(int j=arr.length-1; j>=i; j--){
+            if(!st.isEmpty() && arr[st.peek()] < arr[i]){
+                nse = st.peek();
+                st.push(j);
+            }else{
+                while(!st.isEmpty()){
+                    st.pop();
+                    if(!st.isEmpty() && arr[st.peek()] < arr[i]){
+                        nse = st.peek();
+                        st.push(j);
+                        break;
+                    }
+                }
+                if(st.isEmpty()){
+                    st.push(j);
+                }
+            }
+        }
+        return nse-i;
+    }
+
+    public static int findPSEE(int[] arr, int i){
+        Stack<Integer> st = new Stack<>();
+        int psee = -1;
+        for(int j=0; j<=i; j++){
+            if(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                psee = st.peek();
+                st.push(j);
+            }else{
+                while(!st.isEmpty()){
+                    st.pop();
+                    if(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                        psee = st.peek();
+                        st.push(j);
+                        break;
+                    }
+                }
+                if(st.isEmpty()){
+                    st.push(j);
+                }
+            }
+        }
+        return i-psee;
+    }
+
     public static void main(String[] args) {
         int[] arr = {3,1,2,4};
-        System.out.println(sumSubarrayMins(arr));
+        double sum = 0;
+        int mod = (int)1e9 + 7;
+        for(int i=0; i<arr.length; i++){
+            int nse = findNSE(arr, i);
+            int pse = findPSEE(arr, i);
+            sum += ((nse*pse)*arr[i]);
+        }
+        sum = sum%mod;
+        System.out.println((int)sum);
     }
 }
