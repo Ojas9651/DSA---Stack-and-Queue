6f2a8d462d5efcb80bff2db28995c28481241d1d
Ojas Maurya <ojasmaurya30@gmail.com>
2025-11-30 00:41:16 +0530

    Optimal Solution for Largest Rectangle in Histogram

--- src/Largest_Rectangle_in_Histogram.java
+++ src/Largest_Rectangle_in_Histogram.java
@@ -1,3 +1,5 @@
+/* Optimal solution is down below the Brute One*/
+
 /*
         Brute Force Solution
             1- Find NSE of every height
@@ -73,8 +75,60 @@ public class Largest_Rectangle_in_Histogram {
             return ans;
         }
 
+        /* Optimal Solution
+        *
+        *   we track the previous smaller element
+        *   whenever the st.peek() > arr[i] that means for st.peek() the current
+        *   arr[i] is the next smaller element
+        *   at this point we do area = st.peek() * (arr[i] - st.pop() -1);
+        *   at last do the above till the stack gets empty
+        *
+        * */
+
+    public static int largestRectangleAreaOptimal(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int n = heights.length;
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(!st.isEmpty() && heights[st.peek()] < heights[i]){
+                st.push(i);
+            }else{
+                while(!st.isEmpty()){
+                    int a = heights[st.peek()];
+                    st.pop();
+                    int b = 0;
+                    if(st.isEmpty()){
+                        b = -1;
+                    }else{
+                        b = st.peek();
+                    }
+                    int area = a * (i - b -1);
+                    ans = Math.max(area, ans);
+                    if(!st.isEmpty() && heights[st.peek()] < heights[i]){
+                        st.push(i);
+                        break;
+                    }
+                }
+                if(st.isEmpty()){
+                    st.push(i);
+                }
+            }
+        }
+        while(!st.isEmpty()){
+            int a = heights[st.peek()];
+            int nse = n;
+            st.pop();
+            int pse = 0;
+            if(st.isEmpty()) pse = -1;
+            else pse = st.peek();
+            int area = a * (nse - pse -1);
+            ans = Math.max(area, ans);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] heights = {2,1,5,6,2,3};
-        System.out.println(largestRectangleArea(heights));
+        System.out.println(largestRectangleAreaOptimal(heights));
     }
 }
